5656904f1e7da869500847f53963e1e81ccb41a7
wnbh <30308165+wnbh@users.noreply.github.com>
2020-04-28 22:57:33 +0800

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -21,7 +21,7 @@ echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file finish"
 echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
 
 if [[ $2 -eq 1 ]]; then # single file
-	rclone -v --config="rclone.conf" copy "$3" "DRIVE:$RCLONE_DESTINATION/${relativePath%%/*}"
+	rclone -v --config="rclone.conf" copy "$relativePath" "DRIVE:$RCLONE_DESTINATION" 2>&1
 elif [[ $2 -gt 1 ]]; then # multiple file
 	rclone -v --config="rclone.conf" copy "$topPath" "DRIVE:$RCLONE_DESTINATION/${relativePath%%/*}"
 fi
