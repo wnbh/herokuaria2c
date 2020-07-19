dae72aa58250f7f7373fd6b928896cae16476636
wnbh <30308165+wnbh@users.noreply.github.com>
2020-07-19 10:28:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ if (process.env.HEROKU_APP_NAME) {
 				err ? rej(err) : res(text)
 			)
 		)
-	const APP_URL = `http://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+	const APP_URL = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
 	const preventIdling = () => {
 		request.post(
 			'http://localhost:6800/jsonrpc',
